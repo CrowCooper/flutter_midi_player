1c54e0a9625591caa55fafad3c418ab1b6b6f0d0
Crow Cooper <113761344+CrowCooper@users.noreply.github.com>
2025-03-24 19:15:47 +0800

    Add main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:midi_player/screens/midi_player_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MIDI Player',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MidiPlayerScreen(),
+    );
+  }
+}
